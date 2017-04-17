0ffa3d714cfe55e7e7f78fd0a8c34b9972df1f3b
Adam Lith <adam.lith@gmail.com>
2017-04-17 13:45:36 +0200

    Added a Diractions.All property with all directions.

--- src/Cygni.Snake.Client/Direction.cs
+++ src/Cygni.Snake.Client/Direction.cs
@@ -7,4 +7,9 @@
         Left,
         Right
     }
+
+    public static class Directions
+    {
+        public static Direction[] All = { Direction.Left, Direction.Right, Direction.Up, Direction.Down };
+    }
 }
