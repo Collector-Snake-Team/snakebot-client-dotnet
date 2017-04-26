353df72808698d294906290977e0e7884918d918
Adam Lith <adam.lith@gmail.com>
2017-04-26 19:31:35 +0200

    Adds extension method for getting the opposite of a direction.

--- src/Cygni.Snake.Client/Extensions.cs
+++ src/Cygni.Snake.Client/Extensions.cs
@@ -48,5 +48,22 @@
                 }
             }
         }
+
+        public static Direction Opposite(this Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.Up:
+                    return Direction.Down;
+                case Direction.Down:
+                    return Direction.Up;
+                case Direction.Left:
+                    return Direction.Right;
+                case Direction.Right:
+                    return Direction.Left;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+            }
+        }
     }
 }
